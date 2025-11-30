5ca8f100ef2708451bd0b2f12eb758430eebde31
Alois-PC <alois@schmalwieser.at>
2025-11-30 16:43:04 +0100

    ohne Zupt

--- imu-workshop/schmalwieser_imu_tracking.py
+++ imu-workshop/schmalwieser_imu_tracking.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
-
+import glob
+import os
 
 
 
--- motiontracking/motiontracking.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-speed_series = pd.Series([10, 35, 50, 80, 120], name='speed_kmh')
-print(speed_series)
